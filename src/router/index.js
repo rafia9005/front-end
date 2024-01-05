9518212068be01a7325b2af48db93e38ce889b74
rafia9005 <rafia9005@gmail.com>
2024-01-05 23:51:50 +0700

    pokok commit

---
+++ src/router/index.js
@@ -0,0 +1,44 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import DashboardView from '../views/DashboardView.vue'
+import LoginView from '../views/auth/LoginView.vue'
+import RegisterView from '../views/auth/RegisterView.vue'
+import Logout from '../views/auth/Logout.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: RegisterView
+    },
+    {
+      path: '/logout',
+      name: 'Logout',
+      component: Logout
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: DashboardView
+    },
+  ]
+})
+
+router.beforeEach((to, from, next) => {
+  document.title = to.name;
+  next();
+});
+
+export default router
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import NavbarLayout from '../components/NavbarLayout.vue'
+import HeroLayout from '../components/HeroLayout.vue'
+</script>
+
+<template>
+  <NavbarLayout/>
+  <HeroLayout/>
+</template>
